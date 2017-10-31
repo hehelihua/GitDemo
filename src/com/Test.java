eb894702678c4b1f1b5a954d49d9bb8f6911da16
helihua <435016818@qq.com>
2017-10-31 11:14:53 +0800

    解决冲突

--- src/com/Test.java
+++ src/com/Test.java
@@ -3,7 +3,10 @@ package com;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("test git");
-		System.out.println("test git 分支");
+		System.out.println("test git 分支111");
+		System.out.println("test git 分支222");
+		System.out.println("test git 分支333");
+		System.out.println("test git 分支444");
 		System.out.println("20171031");
 		System.out.println("201710311109");
 	}
